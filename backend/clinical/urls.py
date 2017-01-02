15617337579a115e791bddb08d97b2092f51316e
harsha-headrun <harsha@notemonk.com>
2017-01-02 00:07:52 -0500

    models.py

--- backend/clinical/urls.py
+++ backend/clinical/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls import url
 
 urlpatterns = [
     url(r'^clinicaltrail/', views.clinicaltrail),
+    url(r'^get_wordcloud/', views.get_wordcloud),
     #url(r'^clinicaltrail/(?P<key>.+)/', views.filtersdata),
 ]
